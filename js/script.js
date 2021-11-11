b14f289a04b004bac54e83628944862ff58bfb30
Akulov Maksym <maxim.akulovka@gmail.com>
2021-11-11 20:44:45 +0200

    added scrollToTop, minor fixes

--- js/script.js
+++ js/script.js
@@ -2,3 +2,12 @@ $('.header__burger').click(function(){
     $('.mobile-menu').toggle()
     $('.header__burger').toggleClass('active')
   })
+
+var timeOut;
+function goUp() {
+   var top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
+   if(top > 0) {
+      window.scrollBy(0,-100);
+      timeOut = setTimeout('goUp()',30);
+   } else clearTimeout(timeOut);
+}
