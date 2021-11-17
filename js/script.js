801d53b7a4e8890c0bbe006648bfee9f2f3c8999
Akulov Maksym <maxim.akulovka@gmail.com>
2021-11-17 12:07:53 +0200

    fixing...

--- js/script.js
+++ js/script.js
@@ -1,19 +1,13 @@
 $('.header__burger').click(function(){
-    $('.mobile-menu').toggle()
-    $('.header__burger').toggleClass('active')
+    $('.mobile-menu').toggle();
+    $('.header__burger').toggleClass('active');
   })
 
-var timeOut;
-function goUp() {
-   var top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
-   if(top > 0) {
-      window.scrollBy(0,-100);
-      timeOut = setTimeout('goUp()',30);
-   } else clearTimeout(timeOut);
+var timeOut; 
+function goUp() { 
+   var top = Math.max(document.body.scrollTop,document.documentElement.scrollTop); // Получаем максимальные значиния из двух значений, document.documentElement.scrollTop для кроссбраузерности
+   if(top > 0) { // Если находимся внизу(или, к примеру, в середине экрана), то
+      window.scrollBy(0,-100); // Поднятие вверх на 100px
+      timeOut = setTimeout('goUp()',30); // Каждые 30 милисекунд функция срабатывает и страница подымается вверх
+   } else clearTimeout(timeOut); // Когда мы уже вверху, функцию прекращаем
 }
-
-$("#myModal").on("show", function () {
-  $("body").addClass("modal-open");
-}).on("hidden", function () {
-  $("body").removeClass("modal-open")
-});
