2b082b0636fdd34265ee1a0836f8495bfa97e9ab
Akulov Maksym <maxim.akulovka@gmail.com>
2021-11-18 20:52:24 +0200

    main part

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
+//БУРГЕР МЕНЮ
 $('.header__burger').click(function(){
     $('.mobile-menu').toggle();
     $('.header__burger').toggleClass('active');
+    $('body').toggleClass('lock');
+    $('.mobile-menu').toggleClass('unlock');
   })
 
+//КНОПКА ВВЕРХ
 var timeOut; 
 function goUp() { 
    var top = Math.max(document.body.scrollTop,document.documentElement.scrollTop); // Получаем максимальные значиния из двух значений, document.documentElement.scrollTop для кроссбраузерности
@@ -11,3 +15,23 @@ function goUp() {
       timeOut = setTimeout('goUp()',30); // Каждые 30 милисекунд функция срабатывает и страница подымается вверх
    } else clearTimeout(timeOut); // Когда мы уже вверху, функцию прекращаем
 }
+
+//СВАЙПЕР
+new Swiper('.swiper',{
+
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+    pagination: {
+       el: '.swiper-pagination',
+       clickable: true,
+  },
+    loop: true,
+    
+    slidesPerView: 5,
+    
+    spaceBetween: 0,
+    
+    slidesPerGroup: 2,
+});
